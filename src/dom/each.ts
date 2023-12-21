90ea2f2e86b74e45779bd2a7170b28eb4d973759
Bartek Telec <bartektelec@gmail.com>
2023-12-21 21:57:47 +0100

    remove unused code from example

--- src/dom/each.ts
+++ src/dom/each.ts
@@ -6,12 +6,12 @@ import { computed } from "../signals/computed";
 export const each =
   ($computed: ReturnType<typeof computed>) =>
   <E extends Element, E2 extends Element>(
-    of: () => any[],
+    of: any[],
     cb: (x: any) => Element,
   ) => {
     // TODO add optimization to update one by one DOM node
     const block = $computed(() => {
-      return of().map(cb);
+      return of.map(cb);
     });
 
     return block;
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,24 @@
 import { ctx } from "./signals/ctx";
+import { MutPrimitiveSignal } from "./signals/types";
 
-const {
-  $signal,
-  $computed,
-  $bind,
-  $text,
-  $effect,
-  $attr,
-  $class,
-  $if,
-  $html,
-  $each,
-} = ctx();
+const { $signal, $bind, $html, $each } = ctx();
 
-// Create DOM Nodes
-document.body.innerHTML = `
-<input type='text' id="todo-text"/>
-<button type="button" id="add-button">Add</button>
-<div id="todo-list"></div>`;
+const $bindInput = (
+  a: HTMLElement,
+  signal: MutPrimitiveSignal<string>,
+) => $bind(a as HTMLInputElement, "value", signal, "value");
 
-// Grab references to DOM nodes
-const [inputRef, btnRef, listRef] = (
-  ["#todo-text", "#add-button", "#todo-list"] as const
-).map((x) => document.querySelector(x) as HTMLElement);
+// -------------------- APPLICATION STATE --------------------
 
 type Todo = {
   title: string;
   checked: boolean;
 };
 
-// Declare application state
 const todos = $signal<Todo[]>([]);
 const current = $signal("");
-const addTodo = () => {
+
+function addTodo() {
   const newTodo = {
     title: current.value,
     checked: false,
@@ -40,40 +26,41 @@ const addTodo = () => {
 
   todos.push(newTodo);
   current.value = "";
-};
+}
 
-// Assign state and event handlers to DOM nodes
-$bind(inputRef as HTMLInputElement, "value", current, "value");
-btnRef!.onclick = addTodo;
+// TODO: declaring everything below and binding manually to state and handlers won't be neccessary when templating feature is done
+// -------------------- CREATE DOM NODES --------------------
 
+// Create DOM Nodes
+document.body.innerHTML = `
+<input type='text' id="todo-text"/>
+<button type="button" id="add-button">Add</button>
+<div id="todo-list"></div>
+`;
+
+// Create derived signal that returns a each'd DOM node (raw html) list
 const elements = $each(
-  () => todos,
-  (t) => `<li>Title: ${t.title}</li>`,
+  todos,
+  // @ts-expect-error typing be hard
+  (t, i) => `<li>${i + 1}: ${t.title}</li>`,
 );
 
-const conditionalText = $if(
-  () => todos.length >= 5,
-  "Wow list is getting long man",
-  "Not long list",
-);
+// Grab references to DOM nodes
+const [inputRef, btnRef, listRef] = (
+  ["#todo-text", "#add-button", "#todo-list"] as const
+).map((x) => document.querySelector(x) as HTMLElement);
 
-const cond = $if;
-
+// Render each list item
 $html(
   listRef!,
   () => `
-<p>Here is your list</p>
-${conditionalText.value}
-<ul>
-${elements.value.join("")}
-</ul>
-`,
+    <p>Here is your list</p>
+    <ul>
+      ${elements.value.join("")}
+    </ul>
+  `,
 );
+// -------------------- COMBINE SIGNALS WITH DOM NODES --------------------
 
-const h = (temp: string) => console.log(temp);
-
-h(`
-<div>This is a test: {count}</div>
-<input type="number" bind:value={count}/>
-<button onclick={() => count.value *= 2}>Click</button>
-`);
+$bindInput(inputRef!, current); // two-way bind signal with input value
+btnRef!.onclick = addTodo; // attach an event handler
