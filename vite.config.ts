1a679667de6d2015bf09e34a10d36dac8abfc9be
Bartek Telec <bartektelec@gmail.com>
2024-02-11 14:22:06 +0100

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/pebble/",
   test: {
-    globals: true
-  }
-})
+    globals: true,
+  },
+});
